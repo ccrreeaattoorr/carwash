6d4a5f14960f7777c5250d64512b7243cbfa0913
ccrreeaattoorr <ccrreeaattoorr@gmail.com>
2019-10-27 22:14:32 +0200

    add mock, minor improvements

--- controller_administration.py
+++ controller_administration.py
@@ -1,20 +1,31 @@
-
 import time
 import logging
 import threading
-from controller import Controller
+from controller_mock import Controller
 from subprocess import check_call
 
 
-def thread_function(c):
-    c.move_stepper()
+class ControllerThread:
+
+    def __init__(self, _c1, _c2):
+        self.c1 = _c1
+        self.c2 = _c2
+
+    def thread_function(self):
+        print("thread")
+        self.c1.move_stepper(1000)
+        self.c2.move_stepper(1000, direction="backward")
 
 
 if __name__ == "__main__":
-    c1 = Controller(13, 19, 12, 16, 20, 21).enable_stepper()
-    c2 = Controller(17, 27, 18, 23, 24, 25).enable_stepper()
+    c1 = Controller(13, 19, 12, 16, 20, 21)
+    c2 = Controller(17, 27, 18, 23, 24, 25)
+    c1.enable_stepper()
+    c2.enable_stepper()
 
-    x1 = threading.Thread(target=thread_function, args=c1)
+    ct = ControllerThread(c1, c2)
+
+    x1 = threading.Thread(target=ct.thread_function)
     x1.start()
-    x2 = threading.Thread(target=thread_function, args=c2)
+    x2 = threading.Thread(target=ct.thread_function)
     x2.start()
--- controller_mock.py
+++ controller_mock.py
@@ -1,7 +1,5 @@
 import time
 import argparse
-import RPi.GPIO as GPIO
-#--enable_a 17 --enable_b 27 --coil_A_1_pin 18 --coil_A_2_pin 23 --coil_B_1_pin 24 --coil_B_2_pin 25 --direction backward
 
 
 class Controller:
@@ -16,9 +14,6 @@ class Controller:
         # self.steps = 0
         # self.direction = "forward"
 
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
-
         # Enable GPIO pins for  ENA and ENB for stepper
         self.enable_a = enable_a
         self.enable_b = enable_b
@@ -29,31 +24,20 @@ class Controller:
         self.coil_B_1_pin = coil_b_1_pin
         self.coil_B_2_pin = coil_b_2_pin
 
+        print("Init complete")
+
     def enable_stepper(self):
         # Set pin states
-        GPIO.setup(self.enable_a, GPIO.OUT)
-        GPIO.setup(self.enable_b, GPIO.OUT)
-        GPIO.setup(self.coil_A_1_pin, GPIO.OUT)
-        GPIO.setup(self.coil_A_2_pin, GPIO.OUT)
-        GPIO.setup(self.coil_B_1_pin, GPIO.OUT)
-        GPIO.setup(self.coil_B_2_pin, GPIO.OUT)
-
-        # Set ENA and ENB to high to enable stepper
-        GPIO.output(self.enable_a, True)
-        GPIO.output(self.enable_b, True)
-
         self.is_stepper_enabled = True
         print("is_stepper_enabled: {}".format(self.is_stepper_enabled))
 
     # Function for step sequence
     def set_step(self, w1, w2, w3, w4):
-        GPIO.output(self.coil_A_1_pin, w1)
-        GPIO.output(self.coil_A_2_pin, w2)
-        GPIO.output(self.coil_B_1_pin, w3)
-        GPIO.output(self.coil_B_2_pin, w4)
+        print("step {} {} {} {}".format(w1, w2, w3, w4))
 
     def move_stepper(self, steps, direction="forward", delay=0.0008):
         # loop through step sequence based on number of steps
+        print("steps {} direction: {} delay {}".format(steps, direction, delay))
         if self.is_stepper_enabled and not self.is_moving_now:
             self.is_moving_now = True
             if direction in "forward":
@@ -80,8 +64,6 @@ class Controller:
 
     def disable_stepper(self):
         self.is_stepper_enabled = False
-        GPIO.output(self.enable_a, False)
-        GPIO.output(self.enable_b, False)
         print("is_stepper_enabled: {}".format(self.is_stepper_enabled))
 
 
@@ -101,4 +83,4 @@ if __name__ == "__main__":
     c = Controller(args.enable_a, args.enable_b, args.coil_A_1_pin, args.coil_A_2_pin, args.coil_B_1_pin,
                    args.coil_B_2_pin)
     c.enable_stepper()
-    c.move_stepper(1000)
+    c.move_stepper(3)
