9e7bb8ee35350d676ea4f6c624dc97fb0b2bf5a0
Michael <ccrreeaattoorr@gmail.com>
2020-01-20 22:22:54 +0200

    mecanum api

--- testing-a4988/a4988.py
+++ testing-a4988/a4988.py
@@ -1,3 +1,6 @@
+import sys
+import logging
+import threading
 from time import sleep
 import RPi.GPIO as GPIO
 
@@ -5,42 +8,62 @@ import RPi.GPIO as GPIO
 class A4988:
 
     def __init__(self, direction, step, enable):
-        # DIR = 13   # Direction GPIO Pin
-        # STEP = 19  # Step GPIO Pin
-        # CW = 1     # Clockwise Rotation
-        # CCW = 0    # Counterclockwise Rotation
-        # SPR = 1000   # Steps per Revolution (360 / 7.5)
-        # ENA = 26
+        logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        # GPIO pins
         self.dir = direction
         self.step = step
         self.enable = enable
         self.delay = .001
+        self.is_moving = False
+        logging.info("Pins\nDirection: {} Step: {} Enable: {}".format(self.dir, step, enable))
 
     def enable_stepper(self):
+        self.is_moving = True
         GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
         GPIO.setup(self.dir, GPIO.OUT)
         GPIO.setup(self.step, GPIO.OUT)
         GPIO.setup(self.enable, GPIO.OUT)
-
-    def move(self, step_count, direction):
-        self.enable_stepper()
-
         GPIO.output(self.enable, GPIO.HIGH)
-        GPIO.output(self.dir, direction)
-        for x in range(step_count):
-            GPIO.output(self.step, GPIO.HIGH)
-            sleep(self.delay)
-            GPIO.output(self.step, GPIO.LOW)
-            sleep(self.delay)
+        logging.info("enable_stepper")
+        # logging.info("is_moving {}".format(self.is_moving))
 
-        self.disable_stepper()
+    def move_non_blocking(self, step_count, direction):
+        logging.info("move_non_blocking")
+        self.busy_wait()
+        thread = threading.Thread(target=self.__move, args=(step_count, direction))
+        thread.start()
+
+    def busy_wait(self):
+        while self.is_moving:
+            sleep(0.2)
+            logging.info("Busy wait sleep")
+
+    def __move(self, step_count, direction):
+        if not self.is_moving:
+            self.enable_stepper()
+            logging.info("move")
+
+            GPIO.output(self.dir, direction)
+            for x in range(step_count):
+                GPIO.output(self.step, GPIO.HIGH)
+                sleep(self.delay)
+                GPIO.output(self.step, GPIO.LOW)
+                sleep(self.delay)
+
+            self.disable_stepper()
 
     def disable_stepper(self):
+        logging.info("disable_stepper")
         GPIO.output(self.enable, GPIO.LOW)
-        GPIO.cleanup()
+        self.is_moving = False
+        # GPIO.cleanup()
 
 
 if __name__ == "__main__":
-    c = A4988(13, 19, 26)
-    c.move(1000, 1)
-    c.move(1000, 0)
+    c = A4988(23, 24, 25)
+    logging.info("1 task")
+    c.move_non_blocking(1000, 1)
+    # logging.info("2 task")
+    # c.move_non_blocking(1000, 0)
--- testing-a4988/mecanum.py
+++ testing-a4988/mecanum.py
@@ -1,3 +1,5 @@
+import time
+import threading
 from time import sleep
 from a4988 import A4988
 
@@ -11,18 +13,67 @@ class MecanumWheelsController:
         self.rd_wheel = right_down_wheel
         self.ru_wheel = right_up_wheel
 
-    def move_forward(self):
-        self.ld_wheel.move(1000, 1)
-        self.lu_wheel.move(1000, 1)
-        self.rd_wheel.move(1000, 1)
-        self.ru_wheel.move(1000, 1)
+    def wait(self):
+        while self.ld_wheel.is_moving or self.lu_wheel.is_moving or self.rd_wheel.is_moving or self.ru_wheel.is_moving:
+            print("left down: {} left up: {} right down: {} right up: {}".format(self.ld_wheel.is_moving,
+                                                                                 self.lu_wheel.is_moving,
+                                                                                 self.rd_wheel.is_moving,
+                                                                                 self.ru_wheel.is_moving))
+            time.sleep(0.5)
+
+    def move_forward(self, steps=1000):
+        self.ld_wheel.move_non_blocking(steps, 1)
+        self.lu_wheel.move_non_blocking(steps, 1)
+        self.rd_wheel.move_non_blocking(steps, 1)
+        self.ru_wheel.move_non_blocking(steps, 1)
+        self.wait()
+
+    def move_backward(self, steps=1000):
+        self.ld_wheel.move_non_blocking(steps, 0)
+        self.lu_wheel.move_non_blocking(steps, 0)
+        self.rd_wheel.move_non_blocking(steps, 0)
+        self.ru_wheel.move_non_blocking(steps, 0)
+        self.wait()
+
+    def slide_left(self, steps=1000):
+        self.ld_wheel.move_non_blocking(steps, 1)
+        self.lu_wheel.move_non_blocking(steps, 0)
+        self.rd_wheel.move_non_blocking(steps, 0)
+        self.ru_wheel.move_non_blocking(steps, 1)
+        self.wait()
+
+    def slide_right(self, steps=1000):
+        self.ld_wheel.move_non_blocking(steps, 0)
+        self.lu_wheel.move_non_blocking(steps, 1)
+        self.rd_wheel.move_non_blocking(steps, 1)
+        self.ru_wheel.move_non_blocking(steps, 0)
+        self.wait()
+
+    def rotate_left(self, steps=1000):
+        self.ld_wheel.move_non_blocking(steps, 0)
+        self.lu_wheel.move_non_blocking(steps, 1)
+        self.rd_wheel.move_non_blocking(steps, 1)
+        self.ru_wheel.move_non_blocking(steps, 0)
+        self.wait()
 
 
 if __name__ == "__main__":
-    #        direction, step, enable
+    # direction, step, enable
+
+    # left_down_wheel
     wheel1 = A4988(13, 19, 26)
+    # left_up_wheel
     wheel2 = A4988(16, 20, 21)
+    # right_down_wheel
     wheel3 = A4988(5, 6, 12)
+    # right_up_wheel
     wheel4 = A4988(23, 24, 25)
     m = MecanumWheelsController(wheel1, wheel2, wheel3, wheel4)
-    m.move_forward()
+    m.move_forward(5000)
+    time.sleep(2)
+    m.move_backward(5000)
+    time.sleep(2)
+    m.slide_left(5000)
+    time.sleep(2)
+    m.slide_right(5000)
+
